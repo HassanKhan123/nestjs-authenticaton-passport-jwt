6c41a36fdcc691b3c99de5dc702e1561a0e60965
HassanKhan123 <hk2602809@gmail.com>
2022-11-08 17:46:32 +0500

    added: passport authentication

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'Hassan',
+      username: 'hassan',
+      password: '123',
+    },
+    {
+      id: 2,
+      name: 'Khan',
+      username: 'khan',
+      password: '1234',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
